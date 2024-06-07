1698c753bc2b79ea8283b96c5f972b80ab9222d1
Rohitsharma050 <149524501+Rohitsharma050@users.noreply.github.com>
2024-06-07 13:22:15 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 23.2 MB (18.14%)

---
+++ leetcode-solutions/0151-reverse-words-in-a-string/solution.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        int n=s.length();
+        int i=0;
+        string res="";
+        while(i<n)
+        {
+            while(i<n && s[i]== ' ')
+            i++;
+            if(i>=n)
+            break;
+            int j=i+1;
+            while(j<n && s[j]!=' ')
+            j++;
+            string word=s.substr(i,j-i);
+            if(res.length()==0)
+            res=word;
+            else
+            res=word+" " + res;
+            i=j+1;
+        }
+        return res;
+    }
+};
