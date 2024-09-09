47041c901e33e26e720d38a7e0275d1da5a14374
Rohitsharma050 <149524501+Rohitsharma050@users.noreply.github.com>
2024-09-09 10:57:23 +0000

    [LeetCode Sync] Runtime - 27 ms (5.35%), Memory - 96 MB (18.66%)

---
+++ leetcode-solutions/0004-median-of-two-sorted-arrays/solution.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) 
+    {
+        for(int i=0;i<nums2.size();i++)
+        {
+            nums1.push_back(nums2[i]);
+        }
+        sort(nums1.begin(),nums1.end());
+        int n=nums1.size();
+        int start=0;
+        int end=nums1.size();
+        int mid=start+(end-start)/2;
+        double ans;
+        if(n%2==0)
+        {
+            ans=(double)(nums1[mid]+nums1[mid-1])/2;
+
+        }
+        else{
+            ans=nums1[mid];
+        }
+        return ans;
+        
+    }
+};
