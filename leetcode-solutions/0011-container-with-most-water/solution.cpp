a2b37411c9dd2612ae0538bcf57db3351ce9aa8b
Rohitsharma050 <149524501+Rohitsharma050@users.noreply.github.com>
2024-09-24 04:56:38 +0000

    [LeetCode Sync] Runtime - 70 ms (5.61%), Memory - 61.6 MB (99.98%)

---
+++ leetcode-solutions/0011-container-with-most-water/solution.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int n = height.size();
+        int maxi= 0;
+        int area =0;
+        int left = 0;
+        int right = n-1;
+        while(left<right)
+        {
+            area = (min(height[left],height[right])*(right-left));
+            maxi = max(area,maxi);
+            if(height[left]<height[right])
+           { 
+                left++;
+           }
+            else
+           { 
+                right--;
+            }
+        }
+        return maxi ;
+    }
+};
