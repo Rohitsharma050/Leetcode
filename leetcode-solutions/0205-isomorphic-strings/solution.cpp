b6de8c73d8035e7db288a656884939405b36f5d2
Rohitsharma050 <149524501+Rohitsharma050@users.noreply.github.com>
2024-10-08 19:50:13 +0000

    [LeetCode Sync] Runtime - 9 ms (7.04%), Memory - 9.6 MB (8.08%)

---
+++ leetcode-solutions/0205-isomorphic-strings/solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        map<char ,char>mpp1,mpp2;
+        for(int i=0;i<s.size();i++)
+        {
+            if((mpp1.find(s[i])!=mpp1.end() && mpp1[s[i]]!=t[i]) || (mpp2.find(t[i])!=mpp2.end() && mpp2[t[i]]!=s[i]))
+            {
+                return 0;
+            }
+            mpp1[s[i]] = t[i];
+            mpp2[t[i]] = s[i];;
+        }
+        return true;
+    }
+};
