5c6d65528730e1de82b3c1a0bfc330ccd5d2ee2f
Rohitsharma050 <149524501+Rohitsharma050@users.noreply.github.com>
2024-11-23 15:15:16 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 7.2 MB (100.00%)

---
+++ leetcode-solutions/3625-stone-removal-game/solution.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+  bool canAliceWin(int n) {
+       
+        if (n < 10)
+            return false;
+        if (n == 10)
+            return true;
+        int stonesToRemove = 10;
+        while (n > 0) {
+            n -= stonesToRemove;
+            stonesToRemove--;
+            if (n < stonesToRemove)
+                return true; // Alice wins
+            if (n == stonesToRemove)
+                return false; // Bob wins
+            // stonesToRemove--;
+            n -= stonesToRemove;
+            stonesToRemove--;
+            if (n < stonesToRemove)
+                return false; // Bob wins
+            if (n == stonesToRemove)
+                return true; // Alice wins
+        }
+        return false;
+        
+    }
+};
