283b96662784bb6e6270c4d27b1d22b1e810de71
Rohitsharma050 <149524501+Rohitsharma050@users.noreply.github.com>
2024-12-04 14:15:44 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 16.3 MB (24.49%)

---
+++ leetcode-solutions/0404-sum-of-left-leaves/solution.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+void solve(TreeNode* root,int &sum ,int left)
+{
+    if(root==NULL)
+    return;
+    if(root->left==NULL && root->right==NULL && left == 1)
+    sum+=root->val;
+    solve(root->left,sum,1);
+    solve(root->right,sum,0);
+}
+    int sumOfLeftLeaves(TreeNode* root) {
+        if(root==NULL)
+        return 0;
+        int sum=0;
+        int left =0;
+        solve(root,sum,left);
+        return sum;
+    }
+};
