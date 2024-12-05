7010c40445b0931ea6e5bf1ace0bb8b1919a468d
Rohitsharma050 <149524501+Rohitsharma050@users.noreply.github.com>
2024-12-05 15:25:27 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 10.9 MB (72.28%)

--- leetcode-solutions/0145-binary-tree-postorder-traversal/solution.cpp
+++ leetcode-solutions/0145-binary-tree-postorder-traversal/solution.cpp
@@ -11,22 +11,44 @@
  */
 class Solution {
 public:
-vector<int> postorder(TreeNode* root,vector<int>&ans)
-{
-    if(root)
-    {
-       
-        postorder(root->left,ans);
-        postorder(root->right,ans);
-         ans.push_back(root->val);
-    }
-    return ans;
-}
     vector<int> postorderTraversal(TreeNode* root) {
         vector<int>ans;
         if(root==NULL)
         return ans;
-        postorder(root,ans);
+        while(root)
+        {
+            //check whether root->right exist or not
+            //if does not exist
+            if(root->right==NULL)
+            {
+                ans.push_back(root->val);
+                root = root->left;
+            }
+            //if exist
+            else
+            {
+                //check if right subtree is already traversed
+                TreeNode* curr = root->right;
+                while(curr->left && curr->left!=root)
+                {
+                    curr = curr->left;
+                }
+                //if root->right is not linked to root attech it to root and traverse to root->right 
+                if(curr->left == NULL)
+                {
+                    ans.push_back(root->val);
+                    curr->left=root;
+                    root = root->right;
+                }
+                //if root->right linked to root detach it and traverse to left subtree
+                else
+                {
+                    curr->left = NULL;
+                    root = root->left;
+                }
+            }
+        }
+        reverse(ans.begin(),ans.end());
         return ans;
     }
 };
