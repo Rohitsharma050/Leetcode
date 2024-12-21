34c61de78c46c23adfa023b977d7e70453ecf714
Rohitsharma050 <149524501+Rohitsharma050@users.noreply.github.com>
2024-12-21 08:24:12 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 16.3 MB (97.83%)

---
+++ leetcode-solutions/1040-maximum-binary-tree-ii/solution.cpp
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+TreeNode* solve(TreeNode* &root,int val)
+{
+    if(root==NULL){
+        TreeNode* node = new TreeNode(val);
+        return node;
+    }
+    if(root->val<val)
+    {
+         TreeNode* node = new TreeNode(val);
+         node->left = root;
+         return node;
+    }
+    root->right = solve(root->right,val);
+    return root;
+}
+    TreeNode* insertIntoMaxTree(TreeNode* root, int val) {
+        if(root==NULL)
+        {
+            TreeNode* node= new TreeNode(val);
+            return node;
+        }
+       return  solve(root,val);
+    }
+};
