8ef8b3f94ffd698c6e2399c1417e00a9fd01db97
Rohitsharma050 <149524501+Rohitsharma050@users.noreply.github.com>
2024-12-26 16:19:53 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 26.3 MB (33.63%)

--- leetcode-solutions/0540-single-element-in-a-sorted-array/solution.cpp
+++ leetcode-solutions/0540-single-element-in-a-sorted-array/solution.cpp
@@ -1,21 +1,11 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
-        int left = 0, right = nums.size() - 1;
-        
-        while (left < right) {
-            int mid = left + (right - left) / 2;
-            
-            if (mid % 2 == 1) mid--; // Ensure mid is at an even index
-            
-            if (nums[mid] == nums[mid + 1]) { // Single element is in the right half
-                left = mid + 2;
-            } else { // Single element is in the left half
-                right = mid;
-            }
+        int ans= 0;
+        for(int i=0;i<nums.size();i++)
+        {
+            ans^=nums[i];
         }
-        
-        return nums[left];
+        return ans;
     }
 };
-
