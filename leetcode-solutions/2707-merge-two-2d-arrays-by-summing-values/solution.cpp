a44122da0574cf6664a1bdb07b6bd4263ec4073b
Rohitsharma050 <149524501+Rohitsharma050@users.noreply.github.com>
2024-12-27 16:28:25 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 14.9 MB (29.78%)

---
+++ leetcode-solutions/2707-merge-two-2d-arrays-by-summing-values/solution.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    vector<vector<int>> mergeArrays(vector<vector<int>>& nums1, vector<vector<int>>& nums2) {
+        int n1 = nums1.size();
+        int n2 = nums2.size();
+        int i = 0;
+        int j = 0;
+        vector<vector<int>>ans;
+        while(i<n1 && j<n2)
+        {
+            if(nums1[i][0]<nums2[j][0])
+            {
+                ans.push_back(nums1[i]);
+                i++;
+
+            }
+            else if(nums1[i][0]>nums2[j][0])
+            {
+                ans.push_back(nums2[j]);
+                j++;
+
+            }
+            else
+            {
+                int a=nums1[i][1]+nums2[j][1];
+                vector<int>temp = {nums1[i][0],a};
+                ans.push_back(temp);
+                i++;
+                j++;
+
+            }
+
+        }
+        while(i<n1)
+        {
+            ans.push_back(nums1[i]);
+            i++;
+        }
+        while(j<n2)
+        {
+            ans.push_back(nums2[j]);
+            j++;
+        }
+        return ans;
+    }
+};
