2cc344c4eebe4b9f97773d252d7ecb2507351e4d
Rohitsharma050 <149524501+Rohitsharma050@users.noreply.github.com>
2025-03-19 06:54:39 +0000

    [LeetCode Sync] Runtime - 3 ms (90.40%), Memory - 140 MB (76.50%)

---
+++ leetcode-solutions/3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/solution.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums) {
+        int n = nums.size();
+        int op = 0;
+        for(int i=0;i<n;i++)
+        {
+
+            if(nums[i]==0)
+            {
+                if(i+1>=n || i+2 >=n)
+                return -1;
+                nums[i+1]=1-nums[i+1];
+                nums[i+2] = 1-nums[i+2];
+
+                op++;
+            }
+        }
+        // for(int i = 0;i<n;i++)
+        // {
+        //     if(nums[i]==0)
+        //     return -1;
+        // }
+        return op;
+    }
+};
