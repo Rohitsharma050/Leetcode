ece17820de3f9f72a7267611034ef748bdca06ef
Rohitsharma050 <149524501+Rohitsharma050@users.noreply.github.com>
2025-04-07 10:05:40 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 18.2 MB (17.11%)

--- leetcode-solutions/0034-find-first-and-last-position-of-element-in-sorted-array/solution.cpp
+++ leetcode-solutions/0034-find-first-and-last-position-of-element-in-sorted-array/solution.cpp
@@ -1,45 +1,59 @@
-int upperbound(vector<int>& nums, int target)
-{
-    int s=0;
-	int e= nums.size()-1;
-	int mid,ans=nums.size();
-	while(s<=e)
-	{
-		mid=s+(e-s)/2;
-		if(nums[mid]>target)
-		{
-			ans=mid;
-			e=mid-1;
-		}
-		else
-		s=mid+1;
-
-	}	
-	return ans;
-} 
-int lowerbound(vector<int>& nums, int target)
-{
-    int s=0;
-	int e=nums.size()-1;
-	int mid,ans=nums.size();
-	while(s<=e)
-	{
-		mid=s+(e-s)/2;
-		if(nums[mid]>=target)
-		{ans=mid;
-		e=mid-1;}
-		else
-		s=mid+1;
-	}
-	return ans;
-}
 class Solution {
 public:
+int findLowerIndex(int n ,int target,vector<int>nums)
+{
+    int s = 0;
+    int e = n-1;
+    int ans  =-1;
+    while(s<=e)
+    {
+        int mid = (s+e)/2;
+        if(nums[mid]>=target)
+        {
+            ans = mid;
+            e = mid-1;
+        }
+        else
+        {
+            s = mid+1;
+        }
+    }
+    return ans;
+
+}
+int findUpperIndex(int n ,int target,vector<int>nums)
+{
+    int s = 0;
+    int e = n-1;
+    int ans = -1 ;
+
+    while(s<=e)
+    {
+        int mid = (s+e)/2;
+        if(nums[mid]<=target)
+        {
+            ans = mid;
+           s=mid+1;
+        }
+        else
+        {
+            e = mid-1;
+        }
+    }
+    return ans;
+
+}
     vector<int> searchRange(vector<int>& nums, int target) {
-        int lb=lowerbound(nums,target);
-        int ub=upperbound(nums,target);
-        if(lb==nums.size() || nums[lb]!=target )
+        int lowerIndex = -1;
+        int upperIndex = -1;
+        int n = nums.size();
+        if(n==0)
+        {
         return {-1,-1};
-        return {lb,ub-1};
+        }
+        lowerIndex = findLowerIndex(n,target,nums);
+        upperIndex = findUpperIndex(n,target,nums);
+        if (lowerIndex == -1 || nums[lowerIndex] != target) return {-1, -1};
+        return {lowerIndex,upperIndex};
     }
 };
