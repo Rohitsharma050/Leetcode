b46409fe5a8d91451b7d01d7b28241f48e5f6723
Rohitsharma050 <149524501+Rohitsharma050@users.noreply.github.com>
2025-04-09 10:13:38 +0000

    [LeetCode Sync] Runtime - 7 ms (24.39%), Memory - 13.4 MB (7.29%)

--- leetcode-solutions/0402-remove-k-digits/solution.cpp
+++ leetcode-solutions/0402-remove-k-digits/solution.cpp
@@ -1,37 +1,33 @@
 class Solution {
 public:
-    string removeKdigits(string num, int k) {
-          string st = "";  // Use a string as the stack
-        int n = num.length();
-        
-        // Traverse each digit in the string
-        for (int i = 0; i < n; ++i) {
-            // Pop digits from the stack if they are greater than the current digit
-            // and we still have digits to remove (k > 0)
-            while (!st.empty() && st.back() > num[i] && k > 0) {
-                st.pop_back();
-                k--;
+    string removeKdigits(string nums, int k) {
+        int n = nums.size();
+        if(n==k)
+        return "0";
+        stack<int>st;
+        string ans = "";
+        for(int i = 0;i<n;i++)
+        {
+            while(k>0 && !st.empty() && st.top()>nums[i])
+            {
+                st.pop();
+                ans.pop_back();
+                k--;  
             }
-            // Push the current digit to the stack
-            st.push_back(num[i]);
+             
+            ans+=nums[i];
+            st.push(nums[i]);
         }
-        
-        // If k digits were not removed, remove from the end of the stack
-        while (k > 0) {
-            st.pop_back();
+        while(k>0 && !st.empty())
+        {
+            st.pop();
+            ans.pop_back();
             k--;
         }
-        
-        // Remove leading zeros
-        int start = 0;
-        while (start < st.size() && st[start] == '0') {
-            start++;
+        while(ans.length()>1 && ans[0]=='0')
+        {
+            ans.erase(ans.begin());
         }
-        
-        // Get the resulting number without leading zeros
-        string result = st.substr(start);
-        
-        // If the result is empty, return "0"
-        return result.empty() ? "0" : result;
+        return ans;
     }
 };
