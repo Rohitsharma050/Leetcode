455250b0c21d596f3662afdfb6f7c914040dc419
Rohitsharma050 <149524501+Rohitsharma050@users.noreply.github.com>
2025-04-09 09:33:42 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 14.1 MB (56.29%)

--- leetcode-solutions/1570-final-prices-with-a-special-discount-in-a-shop/solution.cpp
+++ leetcode-solutions/1570-final-prices-with-a-special-discount-in-a-shop/solution.cpp
@@ -1,37 +1,26 @@
 class Solution {
 public:
-    vector<int> finalPrices(vector<int>& prices) {
-        int n = prices.size();
-        vector<int> result(prices); // Copy the original prices
-        stack<int> st;             // Monotonic stack to store indices
+    vector<int> finalPrices(vector<int>& nums) {
+        int n = nums.size();
+        vector<int>ans;
+        stack<int>st;
+        for(int i = n-1;i>=0;i--){
 
-        for (int i = 0; i < n; i++) {
-            // While the stack is not empty and the current price is less than or equal to the price at the top index
-            while (!st.empty() && prices[i] <= prices[st.top()]) {
-                int index = st.top(); // Get the top index
-                st.pop();             // Remove it from the stack
-                result[index] -= prices[i]; // Apply the discount
+            while(!st.empty() && st.top()>nums[i])
+            {
+                st.pop();
             }
-            // Push the current index onto the stack
-            st.push(i);
+            if(st.empty())
+            {
+                ans.push_back(nums[i]);
+            }
+            else
+            {
+                ans.push_back(nums[i]-st.top());
+            }
+            st.push(nums[i]);
         }
-
-        return result;
+        reverse(ans.begin(),ans.end());
+    return ans;
     }
 };
-
-// class Solution {
-// public:
-//     vector<int> finalPrices(vector<int>& prices) {
-//         int n = prices.size();
-//         for (int i = 0; i < n; i++) {   
-//             for (int j = i + 1; j < n; j++) {  // Include all elements after 'i'
-//                 if (prices[i] >= prices[j]) {
-//                     prices[i] -= prices[j];
-//                     break;  // Stop once the first valid discount is found
-//                 }
-//             }
-//         }
-//         return prices;
-//     }
-// };
