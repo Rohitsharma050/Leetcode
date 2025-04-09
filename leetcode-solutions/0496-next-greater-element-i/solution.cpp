bdf3d54364ef65cade58e460158a58bcb886ec06
Rohitsharma050 <149524501+Rohitsharma050@users.noreply.github.com>
2025-04-09 09:44:28 +0000

    [LeetCode Sync] Runtime - 4 ms (22.70%), Memory - 12.7 MB (59.17%)

--- leetcode-solutions/0496-next-greater-element-i/solution.cpp
+++ leetcode-solutions/0496-next-greater-element-i/solution.cpp
@@ -1,29 +1,30 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        vector<int> ans; // Initialize ans with the same size as nums2 and default value -1
-        stack<int> s;
-        map<int,int>mpp;
-        for (int i = nums2.size() - 1; i >= 0; i--) {
-            while (!s.empty() && s.top() <= nums2[i]) {
-                s.pop(); // Pop smaller elements from the stack
+        unordered_map<int,int>mp;
+        int n = nums2.size();
+        stack<int>st;
+        for(int i =n-1;i>=0;i--)
+        {
+            while(!st.empty() && nums2[i]>st.top())
+            {
+                st.pop();
             }
-            
-            if (!s.empty()) {
-                mpp[nums2[i]]=s.top();
-                //ans[i] = s.top(); // The next greater element is at the top of the stack
+            if(!st.empty())
+            {
+                mp[nums2[i]] = st.top();
             }
             else
             {
-                mpp[nums2[i]]=-1;
-            } 
-            s.push(nums2[i]); // Push the current element to the stack
+                mp[nums2[i]] = -1;
+            }
+            st.push(nums2[i]);
         }
-        for(int x=0;x<nums1.size();x++)
+        vector<int>ans;
+        for(int i = 0;i<nums1.size();i++)
         {
-            ans.push_back(mpp[nums1[x]]);
+            ans.push_back(mp[nums1[i]]);
         }
         return ans;
     }
 };
-
