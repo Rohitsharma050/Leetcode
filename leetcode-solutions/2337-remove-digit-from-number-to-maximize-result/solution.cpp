f40f2ff31e278abad8518d851e5bc8e364597e7e
Rohitsharma050 <149524501+Rohitsharma050@users.noreply.github.com>
2025-04-09 17:03:31 +0000

    [LeetCode Sync] Runtime - 2 ms (29.14%), Memory - 8.5 MB (62.57%)

---
+++ leetcode-solutions/2337-remove-digit-from-number-to-maximize-result/solution.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string removeDigit(string number, char digit) {
+        int n = number.length();
+        string ans="";
+        for(int i = 0;i<n;i++)
+        {
+            if(number[i]==digit)
+            {
+                string temp = number;
+                temp.erase(temp.begin()+i);
+                ans = max(ans,temp);
+            }
+        }
+        return ans;
+    }
+};
