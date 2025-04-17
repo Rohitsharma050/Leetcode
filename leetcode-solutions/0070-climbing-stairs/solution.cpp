22bb777f80fa013d303f0aaef3c9b20b0e290f16
Rohitsharma050 <149524501+Rohitsharma050@users.noreply.github.com>
2025-04-17 05:17:09 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 8.6 MB (16.77%)

--- leetcode-solutions/0070-climbing-stairs/solution.cpp
+++ leetcode-solutions/0070-climbing-stairs/solution.cpp
@@ -1,18 +1,15 @@
 class Solution {
 public:
-int getways(int n,vector<int>&dp)
+int solve(int n, vector<int>&dp)
 {
-    if(n<=1)
-    return 1;
+    if(n<=2)
+    return n;
     if(dp[n]!=-1)
-    {
-        return dp[n];
-    }
-    return dp[n] = getways(n-1,dp)+getways(n-2,dp);
+    return dp[n];
+    return dp[n] = solve(n-1,dp)+solve(n-2,dp);
 }
-
     int climbStairs(int n) {
         vector<int>dp(n+1,-1);
-        return getways(n,dp);
+        return solve(n,dp);
     }
 };
