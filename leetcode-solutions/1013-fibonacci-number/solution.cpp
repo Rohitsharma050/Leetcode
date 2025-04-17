9f4a9ad844ba9f1ec603819ddb9ab9101a67c025
Rohitsharma050 <149524501+Rohitsharma050@users.noreply.github.com>
2025-04-17 04:22:13 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 8.2 MB (17.05%)

--- leetcode-solutions/1013-fibonacci-number/solution.cpp
+++ leetcode-solutions/1013-fibonacci-number/solution.cpp
@@ -1,22 +1,15 @@
 class Solution {
 public:
-int getfib(int n)
+int solve(int n,vector<int>&dp)
 {
     if(n<=1)
-    {
-        return n;
-    }
-    int prev1 = 1;
-    int prev2 = 0;
-    for(int i = 2;i<=n;i++)
-    {
-        int curr = prev1+prev2;
-        prev2 = prev1;
-        prev1 = curr;
-    }
-    return prev1;
+    return n;
+    if(dp[n]!=-1)
+    return dp[n];
+    return dp[n] = solve(n-1,dp)+solve(n-2,dp);
 }
     int fib(int n) {
-        return getfib(n);
+        vector<int>dp(n+1,-1);
+        return solve(n,dp);
     }
 };
