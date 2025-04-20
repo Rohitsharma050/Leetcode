804edf0c993c7239fda5be179e769fd492a32c52
Rohitsharma050 <149524501+Rohitsharma050@users.noreply.github.com>
2025-04-20 02:35:46 +0000

    [LeetCode Sync] Runtime - 4 ms (63.58%), Memory - 167.1 MB (84.18%)

---
+++ leetcode-solutions/3732-calculate-score-after-performing-instructions/solution.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    long long calculateScore(vector<string>& instructions, vector<int>& values) {
+          int n = instructions.size();
+        long long score = 0;
+        int i = 0;
+        vector<bool> visited(n, false);
+
+        while (i >= 0 && i < n && !visited[i]) {
+            visited[i] = true;
+            if (instructions[i] == "add") {
+                score += values[i];
+                i++;
+            } else if (instructions[i] == "jump") {
+                i += values[i];
+            } else {
+                // invalid instruction - skip or break
+                break;
+            }
+        }
+
+        return score;
+    }
+};
