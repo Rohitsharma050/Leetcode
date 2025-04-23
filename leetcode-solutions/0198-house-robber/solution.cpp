f1cd5c9aa924be685de424ef6883a94c14f801d7
Rohitsharma050 <149524501+Rohitsharma050@users.noreply.github.com>
2025-04-23 07:07:19 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 10.7 MB (21.59%)

--- leetcode-solutions/0198-house-robber/solution.cpp
+++ leetcode-solutions/0198-house-robber/solution.cpp
@@ -1,18 +1,39 @@
+// class Solution {
+// public:
+// int solve(int i,int n,vector<int>& nums,vector<int>&dp)
+// {
+//     if(i>=n)
+//     return 0;
+//     if(dp[i]!=-1)
+//     return dp[i];
+//     if(i==n-1)
+//     return nums[i];
+    
+//     int take = nums[i]+solve(i+2,n,nums,dp);
+//     int nottake = solve(i+1,n,nums,dp);
+//     return dp[i] = max(take,nottake);
+// }
+//     int rob(vector<int>& nums) {
+//         int n  = nums.size();
+//         vector<int>dp(n+1,-1);
+//         return solve(0,n,nums,dp);
+//     }
+// };
 class Solution {
 public:
     int rob(vector<int>& nums) {
         int n = nums.size();
+        vector<int>dp(n);
+        dp[0] = nums[0];
         if(n==1)
-        return nums[0];
-        int secondprev = nums[0];
-        int prev = max(nums[0],nums[1]);
+        return dp[0];
+        dp[1] = max(nums[0],nums[1]);
         for(int i = 2;i<n;i++)
         {
-            int temp = prev;
-            int one  = nums[i]+secondprev;
-            secondprev = temp;
-            prev = max(secondprev,one);
+            int take=nums[i]+dp[i-2];
+            int nottake = dp[i-1];
+            dp[i] = max(take,nottake);
         }
-        return prev;
+        return max(dp[n-1],dp[n-2]);
     }
 };
