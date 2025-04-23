890e117e6264f63f65fb549f610e6e4bae900749
Rohitsharma050 <149524501+Rohitsharma050@users.noreply.github.com>
2025-04-23 06:42:27 +0000

    [LeetCode Sync] Runtime - 186 ms (45.95%), Memory - 19.8 MB (96.01%)

--- leetcode-solutions/1224-minimum-falling-path-sum-ii/solution.cpp
+++ leetcode-solutions/1224-minimum-falling-path-sum-ii/solution.cpp
@@ -1,27 +1,56 @@
+// class Solution {
+// public:
+// int solve(int i,int prevj,int n,vector<vector<int>>& grid)
+// {
+//     if(i==n)
+//     return 0;
+//     int minvalue = INT_MAX;
+//     for(int j = 0;j<n;j++)
+//     {
+//         if(j!=prevj)
+//         minvalue = min(minvalue,grid[i][j]+solve(i+1,j,n,grid));
+//     }
+//     return minvalue;
+// }
+//     int minFallingPathSum(vector<vector<int>>& grid) {
+//         int n = grid.size();
+//         return solve(0,-1,n,grid);
+//     }
+// };
 class Solution {
 public:
-    int n;
-    
-    int solve(int i, int prevCol, vector<vector<int>>& grid, vector<vector<int>>& dp) {
-        if (i == n) return 0;
-
-        if (dp[i][prevCol + 1] != INT_MAX) return dp[i][prevCol + 1];
-
-        int ans = INT_MAX;
-        for (int j = 0; j < n; j++) {
-            if (j != prevCol) {
-                ans = min(ans, grid[i][j] + solve(i + 1, j, grid, dp));
-            }
-        }
-
-        return dp[i][prevCol + 1] = ans;
-    }
-
     int minFallingPathSum(vector<vector<int>>& grid) {
-        n = grid.size();
-        // prevCol can range from -1 to n-1, so we store in dp[n][n+1]
-        vector<vector<int>> dp(n, vector<int>(n + 1, INT_MAX));
+        int n = grid.size();
+        //create dp of same size;
+        vector<int>prev(n);
+        vector<int>curr(n);
+        //fill 1st row of grid in dp
+        for(int i = 0;i<n;i++)
+        {
+           prev[i] = grid[0][i];
+        }
+        //check  from where index should start
+        for(int i=1;i<n;i++)
+        {
+            for(int j = 0;j<n;j++)
+            {
 
-        return solve(0, -1, grid, dp);
+                //do the same thing already done in memoization 
+                int ans = INT_MAX;
+                for(int k =0;k<n;k++)
+                {
+                    if(j!=k)
+                    ans = min(ans,prev[k]+grid[i][j]);
+                }
+                curr[j]=ans;
+            }
+            prev = curr;
+        }
+        int res = INT_MAX;
+        for(int i = 0;i<n;i++)
+        {
+            res  = min(res,prev[i]);
+        }
+        return res;
     }
 };
