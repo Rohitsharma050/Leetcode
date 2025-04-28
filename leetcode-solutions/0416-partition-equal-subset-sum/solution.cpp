a955261cf10a339f7747d5fa660f61eb3f272c31
Rohitsharma050 <149524501+Rohitsharma050@users.noreply.github.com>
2025-04-28 19:05:11 +0000

    [LeetCode Sync] Runtime - 107 ms (67.63%), Memory - 110.5 MB (31.91%)

--- leetcode-solutions/0416-partition-equal-subset-sum/solution.cpp
+++ leetcode-solutions/0416-partition-equal-subset-sum/solution.cpp
@@ -1,31 +1,62 @@
+// class Solution {
+// public:
+// bool solve(int i,int target,vector<int>& nums,vector<vector<int>>&dp)
+// {
+//     if(i==0)
+//     {
+//         return target==nums[0];
+//     }
+//     if(target==0)
+//     return true;
+//     if(target<0)
+//     return false;
+//     if(dp[i][target]!=-1)
+//     return dp[i][target];
+//     bool nottake = solve(i-1,target,nums,dp);
+//     bool take = false;
+//     if(nums[i]<=target)
+//     {
+//         take = solve(i-1,target-nums[i],nums,dp);
+//     }
+//     return dp[i][target]=take||nottake;
+// }
+//     bool canPartition(vector<int>& nums) {
+//         int n = nums.size();
+//         int totalsum = accumulate(nums.begin(),nums.end(),0);
+//         if(totalsum%2!=0)
+//         return false;
+//         vector<vector<int>>dp(n+1,vector<int>((totalsum/2)+1,-1));
+//         return solve(n-1,totalsum/2,nums,dp);
+//     }
+// };
 class Solution {
 public:
-    vector<vector<int>> dp;
-
-    bool check(int i, int n, vector<int>& nums, int sum, int target) {
-        if (sum > target)
-            return false;
-        if (i == n)
-            return sum == target;
-        if (dp[i][sum] != -1)
-            return dp[i][sum];
-
-        bool take = check(i + 1, n, nums, sum + nums[i], target);
-        bool nottake = check(i + 1, n, nums, sum, target);
-
-        return dp[i][sum] = take || nottake;
-    }
-
     bool canPartition(vector<int>& nums) {
-        int totalsum = accumulate(nums.begin(), nums.end(), 0);
-        if (totalsum % 2 != 0)
-            return false;
-
-        int target = totalsum / 2;
         int n = nums.size();
-        dp = vector<vector<int>>(n, vector<int>(target + 1, -1));
-
-        return check(0, n, nums, 0, target);
+        int totalsum = accumulate(nums.begin(),nums.end(),0);
+        if(totalsum%2!=0)
+        return false;
+        int target = totalsum/2;
+        vector<vector<int>>dp(n,vector<int>(target+1,0));
+        for(int i = 0;i<n;i++)
+        {
+            dp[i][0] = true;
+        }
+        if(nums[0] <= target)
+            dp[0][nums[0]] = true; 
+        for(int i = 1;i<n;i++)
+        {
+            for(int j = 1;j<target+1;j++)
+            {
+                 bool nottake = dp[i-1][j];
+                bool take = false;
+                if(nums[i]<=j)
+                {
+                    take = dp[i-1][j-nums[i]];
+                }
+                dp[i][j]=take||nottake;
+            }
+        }
+        return dp[n-1][target];
     }
 };
-
