c609898dc815d2f8fa2b6fddbd8cf09b67645245
Rohitsharma050 <149524501+Rohitsharma050@users.noreply.github.com>
2025-05-01 17:26:09 +0000

    [LeetCode Sync] Runtime - 8 ms (36.67%), Memory - 121.7 MB (100.00%)

--- leetcode-solutions/3213-count-subarrays-where-max-element-appears-at-least-k-times/solution.cpp
+++ leetcode-solutions/3213-count-subarrays-where-max-element-appears-at-least-k-times/solution.cpp
@@ -1,30 +1,39 @@
 class Solution {
 public:
     long long countSubarrays(vector<int>& nums, int k) {
-        int n = nums.size();
-        int i = 0;
-        int j =0;
-        long long ans = 0;
-        int maxele = *max_element(nums.begin(),nums.end());
-        int maxelecount = 0;
-        while(j<n)
-        {
-            if(nums[j]==maxele)
-            {
-                maxelecount++;
-            }
-            while(maxelecount>=k)
-            {
-                ans+=n-j;
-                if(nums[i]==maxele)
-                {
-                    maxelecount--;
-                }
-                i++;
-            }
-            j++;
+        int n=nums.size();
+        int maxElement = nums[0];  // Initialize maxElement to the first element
 
+    // Iterate through the array to find the maximum element
+    for (int i = 1; i < n; i++) {
+        if (nums[i] > maxElement) {
+            maxElement = nums[i];  // Update maxElement if a larger element is found
         }
-        return ans;
+    }
+
+         int cnt = 0;
+         long long ans = 0;
+         int i=0;
+         int j=0;
+         while(j<nums.size())
+         {  
+            if(nums[j]==maxElement)
+            {
+                cnt++;
+            }
+            while(cnt==k && i<=j)
+            {
+               
+                if(nums[i]==maxElement)
+                {
+                    cnt--;
+                }
+                 i++;
+            }
+           
+            ans+=i;
+            j++;
+         }
+         return ans;
     }
 };
