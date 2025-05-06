099c38abbb70b3ff7f0f9dff979035634648aa4d
Rohitsharma050 <149524501+Rohitsharma050@users.noreply.github.com>
2025-05-06 14:20:49 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 20.5 MB (58.64%)

--- leetcode-solutions/2048-build-array-from-permutation/solution.cpp
+++ leetcode-solutions/2048-build-array-from-permutation/solution.cpp
@@ -1,11 +1,12 @@
 class Solution {
 public:
     vector<int> buildArray(vector<int>& nums) {
-        vector<int>ans;
-        for(int i = 0;i<nums.size();i++)
-        {
-            ans.push_back(nums[nums[i]]);
-        }
-        return ans;
+     int n = nums.size();
+      vector<int>ans(n);
+     for(int i = 0;i<n;i++)
+     {
+        ans[i] = nums[nums[i]];
+     }   
+     return ans;
     }
 };
