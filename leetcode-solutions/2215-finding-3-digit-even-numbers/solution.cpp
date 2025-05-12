a53f7e69dc09b75e1982e7bfeb2e2f7bca6001ea
Rohitsharma050 <149524501+Rohitsharma050@users.noreply.github.com>
2025-05-12 15:40:23 +0000

    [LeetCode Sync] Runtime - 396 ms (14.88%), Memory - 14.1 MB (31.42%)

---
+++ leetcode-solutions/2215-finding-3-digit-even-numbers/solution.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> findEvenNumbers(vector<int>& digits) {
+         set<int> result;
+        int n = digits.size();
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (j == i) continue;
+                for (int k = 0; k < n; ++k) {
+                    if (k == i || k == j) continue;
+
+                    int d1 = digits[i], d2 = digits[j], d3 = digits[k];
+
+                    if (d1 == 0) continue; // leading 0 not allowed
+                    if (d3 % 2 != 0) continue; // must be even
+
+                    int num = d1 * 100 + d2 * 10 + d3;
+                    result.insert(num);
+                }
+            }
+        }
+
+        return vector<int>(result.begin(), result.end());
+    }
+};
