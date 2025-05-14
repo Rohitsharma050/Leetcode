58e1d5653bb3f181ebef697574fb86cd370cceb1
Rohitsharma050 <149524501+Rohitsharma050@users.noreply.github.com>
2025-05-14 17:25:45 +0000

    [LeetCode Sync] Runtime - 9 ms (52.48%), Memory - 44.8 MB (74.17%)

--- leetcode-solutions/0455-assign-cookies/solution.cpp
+++ leetcode-solutions/0455-assign-cookies/solution.cpp
@@ -1,24 +1,27 @@
 class Solution {
 public:
     int findContentChildren(vector<int>& g, vector<int>& s) {
-        // Sort both arrays
-        sort(g.begin(), g.end());
-        sort(s.begin(), s.end());
-        
-        int i = 0; // Pointer for children
-        int j = 0; // Pointer for cookies
-        
-        // Assign cookies to children
-        while (i < g.size() && j < s.size()) {
-            if (s[j] >= g[i]) {
-                // If the cookie satisfies the child's greed, move to the next child
+        int gSize = g.size();
+        int sSize = s.size();
+        sort(g.begin(),g.end());
+        sort(s.begin(),s.end());
+        int i = 0;
+        int j = 0;
+        int ans = 0;
+        while(i<gSize && j<sSize)
+        {
+            if(g[i]<=s[j])
+            {
+                ans++;
                 i++;
+                j++;
             }
-            // Move to the next cookie in either case
-            j++;
+            else if(g[i]>s[j])
+            {
+                j++;
+            }
+            
         }
-        
-        return i; // Number of satisfied children
+        return ans;
     }
 };
-
