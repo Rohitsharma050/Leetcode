48f1eaffef3486c5c5e342c4db9e6fc9cde3546e
Rohitsharma050 <149524501+Rohitsharma050@users.noreply.github.com>
2025-05-22 05:01:28 +0000

    [LeetCode Sync] Runtime - 82 ms (48.34%), Memory - 76.1 MB (7.82%)

---
+++ leetcode-solutions/0516-longest-palindromic-subsequence/solution.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+int solve(int i,int j,string &s,string &t,vector<vector<int>>&dp)
+{
+    if(i<0 || j<0)
+    {
+        return 0;
+    }
+    if(dp[i][j]!=-1)
+    return dp[i][j];
+    if(s[i]==t[j])
+    {
+        return dp[i][j] = 1+solve(i-1,j-1,s,t,dp);
+    }
+    else
+    {
+        return dp[i][j] = max(solve(i-1,j,s,t,dp),solve(i,j-1,s,t,dp));
+    }
+}
+    int longestPalindromeSubseq(string s) {
+        int n = s.length();
+        string t = s;
+        reverse(t.begin(),t.end());
+        vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
+        return solve(n-1,n-1,s,t,dp);
+
+    }
+};
