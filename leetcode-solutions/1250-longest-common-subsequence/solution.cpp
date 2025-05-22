0046fc35b430bf05bcf8944b9dd183ca2a864ceb
Rohitsharma050 <149524501+Rohitsharma050@users.noreply.github.com>
2025-05-22 07:00:06 +0000

    [LeetCode Sync] Runtime - 12 ms (94.56%), Memory - 9.1 MB (96.51%)

--- leetcode-solutions/1250-longest-common-subsequence/solution.cpp
+++ leetcode-solutions/1250-longest-common-subsequence/solution.cpp
@@ -1,50 +1,57 @@
 // class Solution {
 // public:
-// int solve(int i,int j,string &text1, string &text2,vector<vector<int>>&dp)
-// {
-//     if(i<0 || j<0)
-//     return 0;
-//     if(dp[i][j]!=-1)
-//     return dp[i][j];
-//     if(text1[i]==text2[j])
-//     {
-//         return dp[i][j]=1+solve(i-1,j-1,text1,text2,dp);
-//     }
-//     return dp[i][j] = max(solve(i-1,j,text1,text2,dp),solve(i,j-1,text1,text2,dp));
-// }
 //     int longestCommonSubsequence(string text1, string text2) {
-//         int n1 = text1.size();
-//         int n2 = text2.size();
-//         vector<vector<int>>dp(n1+1,vector<int>(n2+1,-1));
-//         return solve(n1-1,n2-1,text1,text2,dp);
+//         int n = text1.length();
+//         int m = text2.length();
+//         vector<vector<int>>dp(n+1,vector<int>(m+1));
+//         for(int i = 0;i<n;i++)
+//         {
+//             dp[i][0] = 0;
+//         }
+//         for(int j = 0;j<m;j++)
+//         {
+//             dp[0][j] = 0;
+//         }
+//         for(int i =1;i<=n;i++)
+//         {
+//             for(int j = 1;j<=m;j++)
+//             {
+//                 if(text1[i-1]==text2[j-1])
+//                 {
+//                     dp[i][j] = 1+dp[i-1][j-1];
+//                 }
+//                 else
+//                 {
+//                     dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
+//                 }
+//             }
+//         }
+//         return dp[n][m];
 //     }
 // };
 class Solution {
 public:
     int longestCommonSubsequence(string text1, string text2) {
-         int n1 = text1.size();
-        int n2 = text2.size();
-        vector<vector<int>>dp(n1+1,vector<int>(n2+1,0));
-        for(int i = 0;i<=n2;i++)
+        int n = text1.length();
+        int m = text2.length();
+        vector<int>prev(m+1,0);
+        vector<int>curr(m+1,0);
+        for(int i = 1;i<=n;i++)
         {
-            dp[0][i] = 0;
-        }
-        for(int i = 0;i<=n1;i++)
-        {
-            dp[i][0] = 0;
-        }
-        for(int i = 1;i<=n1;i++)
-        {
-            for(int j = 1;j<=n2;j++)
+            for(int j = 1;j<=m;j++)
             {
-                 if(text1[i-1]==text2[j-1])
+                if(text1[i-1]==text2[j-1])
                 {
-                    dp[i][j]=1+dp[i-1][j-1];
+                    curr[j] = 1+prev[j-1];
                 }
-                 else
-                 dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
+                else
+                {
+                    curr[j] = max(prev[j],curr[j-1]);
+                }
             }
+            prev = curr;
         }
-        return dp[n1][n2];
+
+return prev[m];
     }
 };
