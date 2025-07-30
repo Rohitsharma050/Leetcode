2dfaa0073fc004ec310e521d0d82005730692540
Rohitsharma050 <149524501+Rohitsharma050@users.noreply.github.com>
2025-07-30 10:52:08 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 86 MB (99.11%)

--- leetcode-solutions/2503-longest-subarray-with-maximum-bitwise-and/solution.cpp
+++ leetcode-solutions/2503-longest-subarray-with-maximum-bitwise-and/solution.cpp
@@ -1,33 +1,21 @@
 class Solution {
 public:
     int longestSubarray(vector<int>& nums) {
-        int n = nums.size();
-        if (n == 0) return 0;  
-        if (n == 1) return 1; 
-
-        int max_value = 0;
-        for (int i = 0; i < n; i++) {
-            if (nums[i] > max_value)
-                max_value = nums[i];  
-        }
-
-        int maxans = 0;
-        int current_length = 0;
-
-        for (int i = 0; i < n; i++) {
-            if (nums[i] == max_value) {
-                current_length++;  
-            } else {
-                if (current_length > maxans)
-                    maxans = current_length;  
-                current_length = 0;  
+        int maxElement = *max_element(nums.begin(),nums.end());
+        int n= nums.size();
+        int ans= 0;
+        int length = 0;
+        for(int i = 0 ;i<n;i++)
+        {
+            if(nums[i]==maxElement)
+            {
+                ans++;
+                length = max(ans,length);
+            }
+            else{
+                ans = 0;
             }
         }
-
-        if (current_length > maxans)
-            maxans = current_length;
-
-        return maxans;
+        return length;
     }
 };
-
