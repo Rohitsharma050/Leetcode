593e739fbee459d5ea51cdef16603fc93f26d2ce
Rohitsharma050 <149524501+Rohitsharma050@users.noreply.github.com>
2025-08-20 04:57:30 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 15.2 MB (32.30%)

--- leetcode-solutions/0033-search-in-rotated-sorted-array/solution.cpp
+++ leetcode-solutions/0033-search-in-rotated-sorted-array/solution.cpp
@@ -1,35 +1,36 @@
 class Solution {
 public:
-     int search(vector<int>& nums, int target) {
-     int n=nums.size();
-     int s=0;
-     int e=n-1;
-     int mid;
-     while(s<=e)
-     {  mid=(e+s)/2;
+int solve(int i,int n,vector<int>&nums,int target){
+    int s = 0;
+    int e = n-1;
+    while(s<=e)
+    {
+        int mid = (s+e)/2;
         if(nums[mid]==target)
         return mid;
-        if(nums[0]<=nums[mid])
+        if(nums[mid]>=nums[s])
         {
-            if(target>=nums[0] && target<=nums[mid])
+            if(nums[s]<=target && nums[mid]>=target)
             {
-                e=mid-1;
+                e = mid-1;
             }
             else
-            s=mid+1;
-
-        }
-        else
-        {
-            if(nums[mid]<=target && target <=nums[n-1])
             {
-                s=mid+1;
+                s = mid+1;
             }
-            else
-            e=mid-1;
         }
-
-     }
-     return -1;
-     }
+       else {
+                if(nums[mid] <= target && target <= nums[e]) {
+                    s = mid + 1;
+                } else {
+                    e = mid - 1;
+                }
+            }
+        }
+        return -1;
+}
+    int search(vector<int>& nums, int target) {
+        int n = nums.size();
+        return solve(0,n,nums,target);
+    }
 };
