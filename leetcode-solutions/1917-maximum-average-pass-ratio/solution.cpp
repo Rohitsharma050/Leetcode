bf496cd9f242e3c5598310cbe93197a9c2d4e927
Rohitsharma050 <149524501+Rohitsharma050@users.noreply.github.com>
2025-09-01 12:19:01 +0000

    [LeetCode Sync] Runtime - 268 ms (83.13%), Memory - 97.7 MB (74.17%)

--- leetcode-solutions/1917-maximum-average-pass-ratio/solution.cpp
+++ leetcode-solutions/1917-maximum-average-pass-ratio/solution.cpp
@@ -1,50 +1,34 @@
 class Solution {
 public:
-    typedef pair<double, pair<double, double>> p;
- double calcImprovement(double p, double t)
-{
-     return ((p + 1) / (t + 1)) - (p / t);
-}
-    double maxAverageRatio(vector<vector<int>>& classes, int k) {
+    double maxAverageRatio(vector<vector<int>>& classes, int extraStudents) {
         int n = classes.size();
-        // Max heap based on improvement in pass ratio
-       
+        priority_queue<pair<double,int>> pq;
 
-        priority_queue<p> pq;
         for (int i = 0; i < n; i++) {
-            double p = classes[i][0]; // Passes
-            double t = classes[i][1]; // Total students
-            double improvement = calcImprovement(p, t);
-            pq.push({improvement, {p, t}});
+            int a = classes[i][0];
+            int b = classes[i][1];
+            double diff = (double)(a+1)/(b+1) - (double)a/(double)b;
+            pq.push({diff, i});
         }
 
-        // Distribute extra students
-        while (k--) {
-            auto top = pq.top();
-            pq.pop();
+        for (int i = 0; i < extraStudents; i++) {
+            auto top = pq.top(); pq.pop();
+            int idx = top.second;
 
-            double p = top.second.first; // Current passes
-            double t = top.second.second; // Current total
-            p++; // Add one pass
-            t++; // Add one total student
+            classes[idx][0]++;
+            classes[idx][1]++;
 
-            // Recalculate improvement and push back doubleo the heap
-            double improvement = calcImprovement(p, t);
-            pq.push({improvement, {p, t}});
+            int a = classes[idx][0];
+            int b = classes[idx][1];
+            double newDiff = (double)(a+1)/(b+1) - (double)a/(double)b;
+            pq.push({newDiff, idx});
         }
 
-        // Calculate final average pass ratio
         double ans = 0.0;
-        while (!pq.empty()) {
-            auto top = pq.top();
-            pq.pop();
-
-            double p = top.second.first;
-            double t = top.second.second;
-            ans += p / t; // Add the pass ratio of this class
+        for (int i = 0; i < n; i++) {
+            ans += (double)classes[i][0] / classes[i][1];
         }
-
-        return ans / n; // Return the average
+        return ans / n;
     }
 };
 
