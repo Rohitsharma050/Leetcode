468eb4ce48246f644b02faa7a100f4ea2cc5ef78
Rohitsharma050 <149524501+Rohitsharma050@users.noreply.github.com>
2025-09-04 16:01:09 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 8.6 MB (18.31%)

--- leetcode-solutions/3830-find-closest-person/solution.cpp
+++ leetcode-solutions/3830-find-closest-person/solution.cpp
@@ -1,11 +1,10 @@
 class Solution {
 public:
     int findClosest(int x, int y, int z) {
-       int dist1 = abs(x - z);
-    int dist2 = abs(y - z);
-
-    if (dist1 < dist2) return 1;
-    else if (dist2 < dist1) return 2;
-    else return 0; // both reach at the same time
+        int a = abs(z-x);
+        int b = abs(y-z);
+        if(a==b)
+        return 0;
+        return a<b?1:2;
     }
 };
