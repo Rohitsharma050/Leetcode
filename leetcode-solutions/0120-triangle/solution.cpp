9bf2eea5be87826ee6bb370eda2d95e8794c37d2
Rohitsharma050 <149524501+Rohitsharma050@users.noreply.github.com>
2025-09-25 13:50:07 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 13.2 MB (46.07%)

--- leetcode-solutions/0120-triangle/solution.cpp
+++ leetcode-solutions/0120-triangle/solution.cpp
@@ -1,19 +1,16 @@
 class Solution {
 public:
+int solve(int i,int j,int n,vector<vector<int>>& triangle,vector<vector<int>>&dp)
+{
+    if(i==n)
+    return 0;
+    if(dp[i][j]!=1e6)
+    return dp[i][j];
+    return dp[i][j] = triangle[i][j]+ min(solve(i+1,j,n,triangle,dp),solve(i+1,j+1,n,triangle,dp));
+}
     int minimumTotal(vector<vector<int>>& triangle) {
         int n = triangle.size();
-        vector<vector<int>>dp(n+1,vector<int>(n+1,0));
-        for(int i = 0;i<n;i++)
-        {
-            dp[n-1][i] = triangle[n-1][i];
-        }
-        for(int i = n-2;i>=0;i--)
-        {
-            for(int j = triangle[i].size()-1;j>=0;j--)
-            {
-                dp[i][j] = triangle[i][j] + min(dp[i+1][j],dp[i+1][j+1]); 
-            }
-        }
-        return dp[0][0];
+        vector<vector<int>>dp(n,vector<int>(n,1e6));
+        return solve(0,0,n,triangle,dp);
     }
 };
