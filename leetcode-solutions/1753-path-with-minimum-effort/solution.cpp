7d45d0b52c27339ad6d94f7c431918dd7502d18a
Rohitsharma050 <149524501+Rohitsharma050@users.noreply.github.com>
2025-10-06 18:58:18 +0000

    [LeetCode Sync] Runtime - 51 ms (66.99%), Memory - 24.6 MB (46.93%)

--- leetcode-solutions/1753-path-with-minimum-effort/solution.cpp
+++ leetcode-solutions/1753-path-with-minimum-effort/solution.cpp
@@ -1,41 +1,42 @@
 class Solution {
 public:
 typedef pair<int,pair<int,int>> P;
-bool valid(int i,int j, int n,int m)
+bool isValid(int i,int j,int n,int m)
 {
-    return i>=0 && i<n && j>=0 && j<m;
+    return i<n && j<m && i>=0 && j>=0;
 }
+vector<vector<int>>dir = {{-1,0},{0,1},{1,0},{0,-1}};
     int minimumEffortPath(vector<vector<int>>& heights) {
         int n = heights.size();
         int m = heights[0].size();
-        priority_queue<P,vector<P>,greater<P>> pq;
+        vector<vector<int>>cost(n,vector<int>(m,INT_MAX));
+        priority_queue<P,vector<P>,greater<P>>pq;
         pq.push({0,{0,0}});
-        vector<vector<int>>dist(n,vector<int>(m,INT_MAX));
-        dist[0][0] = 0;
         while(!pq.empty())
         {
             auto it = pq.top();
             pq.pop();
-            int h = it.first;
+            int currDiff = it.first;
             int r = it.second.first;
             int c = it.second.second;
-            int row[4] = {0,0,1,-1};
-            int col[4] = {1,-1,0,0};
+            if(r==n-1 && c == m-1)
+            return currDiff;
             for(int i = 0;i<4;i++)
             {
-                int adjrow = r+row[i];
-                int adjcol = c+col[i];
-                if(valid(adjrow,adjcol,n,m))
-                {
-                    int diff = abs(heights[adjrow][adjcol]-heights[r][c]);
-                    int maxdiff = max(diff,h);
-                    if(maxdiff<dist[adjrow][adjcol])
-                    {dist[adjrow][adjcol] = maxdiff;
-                    pq.push({maxdiff,{adjrow,adjcol}});}
-                }
-            }
-        }
-        return dist[n-1][m-1];
+                int newr = r + dir[i][0];
+                int newc = c + dir[i][1];
+                if(isValid(newr,newc,n,m))
+{
+if (max(abs(heights[r][c] - heights[newr][newc]), currDiff) < cost[newr][newc])
 
+                {
+                    cost[newr][newc] = max(abs(heights[r][c] - heights[newr][newc]), currDiff);
+                    pq.push({max(abs(heights[r][c] - heights[newr][newc]), currDiff),{newr,newc}});
+                }
+            }}
+
+        }
+        return -1;
+        
     }
 };
