31ad7407972c63607c40cb79e66b94221928d59e
Rohitsharma050 <149524501+Rohitsharma050@users.noreply.github.com>
2025-10-23 13:24:25 +0000

    [LeetCode Sync] Runtime - 3 ms (71.45%), Memory - 9.9 MB (44.32%)

--- leetcode-solutions/3768-check-if-digits-are-equal-in-string-after-operations-i/solution.cpp
+++ leetcode-solutions/3768-check-if-digits-are-equal-in-string-after-operations-i/solution.cpp
@@ -1,19 +1,14 @@
 class Solution {
 public:
     bool hasSameDigits(string s) {
-        int n = s.length();
-        while(s.length()>2)
-            {
-                string ans = "";
-            for(int  i =0;i<s.length()-1;i++)
-              {  int d1 = s[i]-'0';
-                int d2 = s[i+1]-'0';
-                int newd = (d1+d2)%10;
-                ans+=to_string(newd);
-               
-              }
-                 s = ans;
-            }
-         return (s.length() == 2 && s[0] == s[1]);  ;
+        while (s.length() > 2) {
+        string newStr = "";
+        for (size_t i = 0; i < s.length() - 1; i++) {
+            int sum = (s[i] - '0') + (s[i + 1] - '0');
+            newStr += (sum % 10) + '0';
+        }
+        s = newStr;
+    }
+    return s[0] == s[1];
     }
 };
