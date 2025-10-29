8ff7cc50a6a07ebe20bd32aac85b59ea7a93f431
Rohitsharma050 <149524501+Rohitsharma050@users.noreply.github.com>
2025-10-29 13:08:54 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 8.3 MB (57.11%)

--- leetcode-solutions/3676-smallest-number-with-all-set-bits/solution.cpp
+++ leetcode-solutions/3676-smallest-number-with-all-set-bits/solution.cpp
@@ -1,9 +1,10 @@
 class Solution {
 public:
     int smallestNumber(int n) {
-        int totalBits = 0;
-        totalBits = static_cast<int>(log2(n)) + 1;
-        int ans = pow(2,totalBits)-1;
-        return ans;
+        int x = 1;
+        while (x < n) {
+            x = x * 2 + 1;
+        }
+        return x;
     }
 };
