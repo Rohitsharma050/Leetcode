fbd2a58987ada25a21bc5ecbd999198360fa8960
Rohitsharma050 <149524501+Rohitsharma050@users.noreply.github.com>
2025-10-31 04:26:43 +0000

    [LeetCode Sync] Runtime - 3 ms (41.29%), Memory - 20.6 MB (36.86%)

--- leetcode-solutions/0547-number-of-provinces/solution.cpp
+++ leetcode-solutions/0547-number-of-provinces/solution.cpp
@@ -1,38 +1,39 @@
 class Solution {
 public:
-    void dfs(int node, vector<int>& visited, vector<int> adj[]) {
-        visited[node] = 1;
-        for (int n : adj[node]) {
-            if (!visited[n]) {
-                dfs(n, visited, adj);
-            }
+void dfs(int node,vector<vector<int>>&adj,vector<int>&vis)
+{
+    vis[node] = 1;
+    for(auto neigh:adj[node])
+    {
+        if(!vis[neigh])
+        {
+            dfs(neigh,adj,vis);
         }
     }
-
-    int findCircleNum(vector<vector<int>>& adjMat) {
-        int v = adjMat.size();
-        vector<int> adj[v];
-        
-        // Construct adjacency list
-        for (int i = 0; i < v; i++) {
-            for (int j = 0; j < v; j++) {
-                if (adjMat[i][j]) {
-                    adj[i].push_back(j);
+}
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int n = isConnected.size();
+        vector<int>vis(n,0);
+        int count = 0;
+        vector<vector<int>>adj(n);
+        for(int i = 0;i<n;i++)
+        {
+            for(int j = 0;j<n;j++)
+            {
+                if(isConnected[i][j]==1)
+                {
+                    adj[i].push_back(j); 
                 }
             }
         }
-        
-        int count = 0;
-        vector<int> visited(v, 0);
-
-        // Perform DFS to count connected components
-        for (int i = 0; i < v; i++) {
-            if (!visited[i]) {
-                dfs(i, visited, adj);
+        for(int i = 0;i<n;i++)
+        {
+            if(!vis[i])
+            {
                 count++;
+                dfs(i,adj,vis);
             }
         }
         return count;
     }
 };
-
