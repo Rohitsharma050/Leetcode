a484db44db29a80e1fc23b7e7d8ea3c87bd0049a
Rohitsharma050 <149524501+Rohitsharma050@users.noreply.github.com>
2025-11-19 05:02:11 +0000

    [LeetCode Sync] Runtime - 3 ms (43.33%), Memory - 15.9 MB (8.14%)

---
+++ leetcode-solutions/2274-keep-multiplying-found-values-by-two/solution.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int findFinalValue(vector<int>& nums, int original) {
+        int n = nums.size();
+        int ans = original;
+        unordered_set<int>st(nums.begin(),nums.end());
+        while(st.find(original)!=st.end())
+        {
+            original*=2;
+        }
+        return original;
+    }
+};
