12f027a212db9d510fea9a7aac16324e151c2812
Rohitsharma050 <149524501+Rohitsharma050@users.noreply.github.com>
2025-11-21 16:32:59 +0000

    [LeetCode Sync] Runtime - 163 ms (37.06%), Memory - 15.9 MB (46.19%)

--- leetcode-solutions/2059-unique-length-3-palindromic-subsequences/solution.cpp
+++ leetcode-solutions/2059-unique-length-3-palindromic-subsequences/solution.cpp
@@ -1,34 +1,33 @@
 class Solution {
 public:
     int countPalindromicSubsequence(string s) {
-        int n = s.length();
-        unordered_set<char>uniqueLetters;
-        vector<pair<int,int>>letters(26,{-1,-1});
-        for(int i=0;i<n;i++)
-        {
-            char ch = s[i];
-            int index = ch -'a';
-            if(letters[index].first==-1)
+          int n = s.length();
+          int ans = 0;
+          unordered_map<char,pair<int,int>>mp;
+          for(int i = 0;i<n;i++)
+          {
+            if(mp.find(s[i])!=mp.end())
             {
-                letters[index].first = i;
+                mp[s[i]].second = i;
             }
-            letters[index].second = i;
-        }
-
-        int result = 0;
-        for(int i = 0;i<26;i++)
-        {
-            int firstIndex = letters[i].first;
-            int lastIndex  = letters[i].second;
-            if(firstIndex==-1)
-            continue;
-            unordered_set<char>middleLetters;
-            for(int k = firstIndex+1;k<lastIndex;k++)
-            {
-                middleLetters.insert(s[k]);
+            else{
+                mp[s[i]].first = i;
             }
-            result+=middleLetters.size();
-        }
-        return result;
+          }
+         
+          for(auto it:mp)
+          {
+            int start = it.second.first;
+            int end = it.second.second;
+            if(end-start>=2)
+            {    unordered_set<char>st;
+                for(int i = start+1;i<end;i++)
+                {
+                    st.insert(s[i]);
+                }
+                ans+=st.size();
+            }
+          }
+        return ans;
     }
 };
