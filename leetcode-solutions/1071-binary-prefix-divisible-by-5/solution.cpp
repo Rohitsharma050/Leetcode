ce3510c102761ad1ff54e21d6de34e414cd0aa11
Rohitsharma050 <149524501+Rohitsharma050@users.noreply.github.com>
2025-11-24 04:09:12 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 18.5 MB (9.12%)

---
+++ leetcode-solutions/1071-binary-prefix-divisible-by-5/solution.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<bool> prefixesDivBy5(vector<int>& nums) {
+      int n = nums.size();
+      vector<bool>ans;
+      long long decimal = 0;
+      for(int i = 0;i<n;i++)
+      {
+        decimal= (decimal*2+nums[i])%5;
+        if(decimal==0)
+        {
+            ans.push_back(true);
+        }
+        else
+        {
+            ans.push_back(false);
+        }
+      }
+    return ans;
+    }
+};
